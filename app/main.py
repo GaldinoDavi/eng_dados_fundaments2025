0ff3ef12cd29a2180c9653ca1db17cedf718de78
David <davi.hg123@gmail.com>
2025-07-11 00:14:58 -0300

    Adicionado load

--- app/main.py
+++ app/main.py
@@ -1,4 +1,15 @@
 from pipeline.extract import extract_from_excel
+from pipeline.transform import contact_data_frames
+from pipeline.load import load_excel
+
+if __name__ == '__main__':
+    listas_data_frame = extract_from_excel("data/input")
+    print(type(listas_data_frame))
+    data_frame = contact_data_frames(listas_data_frame)
+    print(type(data_frame))
+    load_excel(data_frame, "data/output", "output")
+    print()
+
 
 listas_data_frame = extract_from_excel("data/input")
 
